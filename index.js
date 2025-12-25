c4a0f1412f4e228f18b26b27a89fc2c21a7b7c80
smith250825-png <smith250825@gmail.com>
2025-12-25 16:35:32 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ESKETIT API is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
